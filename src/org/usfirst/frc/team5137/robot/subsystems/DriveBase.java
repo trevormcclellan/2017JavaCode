4c5429ca0b437baaa70b817e7f9320cc2186a1ce
rohan <rohan@Rao-Lenovo-Ubuntu>
2017-02-04 16:39:19 -0800

    Added pivot x and y and centering for x and y value from networktables

--- src/org/usfirst/frc/team5137/robot/subsystems/DriveBase.java
+++ src/org/usfirst/frc/team5137/robot/subsystems/DriveBase.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team5137.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import org.usfirst.frc.team5137.robot.*;
 import edu.wpi.first.wpilibj.*;
 import org.usfirst.frc.team5137.robot.commands.*;
@@ -13,6 +14,13 @@ public class DriveBase extends Subsystem {
 	SpeedController rightMotor = RobotMap.driveBaseRightMotor;
 	RobotDrive chassis = RobotMap.driveBaseChassis;
 	ADXRS450_Gyro gyro = RobotMap.gyro;
+	NetworkTable aimingTable = NetworkTable.getTable("SmartDashboard");
+	public boolean isXCentered = false;
+	public boolean isYCentered = false;
+	private static double xCoord;
+	private static double yCoord;
+	double centerX;
+	double centerY;
 	
 	double Kp = 0.03;
     // Put methods for controlling this subsystem
@@ -31,11 +39,57 @@ public class DriveBase extends Subsystem {
         chassis.drive(-1.0, -angle*Kp); // drive towards heading 0
 	}
 	
+	public void pivotLeft() {
+		leftMotor.set(0.3);
+		rightMotor.set(-0.3);
+	}
+	
+	public void pivotRight() {
+		leftMotor.set(-0.3);
+		rightMotor.set(0.3);
+	}
+
+	public void centerX() {
+		isXCentered = false;
+		xCoord = aimingTable.getNumber("COG_X", 0);
+		if (xCoord == 0) {
+			stop();
+		} 
+		else if (xCoord > centerX + 2) {
+			pivotRight();
+		}
+		else if (xCoord < centerX - 2) {
+			pivotLeft();
+		}
+		else {
+			isXCentered = true;
+			stop();
+		}
+	}
+	
+public void centerY() {
+		isYCentered = false;
+		yCoord = aimingTable.getNumber("COG_y", 0);
+		if (yCoord == 0) {
+			stop();
+		}
+		else if (yCoord > centerY + 2) {
+			driveStraight(-0.3);
+		}
+		else if (yCoord < centerY - 2) {
+			driveStraight(0.3);
+		}
+		else {
+			isYCentered = true;
+			stop();
+		}
+	}
+
 	public void stop() {
 		chassis.drive(0, 0);
 		gyro.reset();
 	}
-
+	
     public void initDefaultCommand() {
     	setDefaultCommand(new Drive());
         // Set the default command for a subsystem here.
